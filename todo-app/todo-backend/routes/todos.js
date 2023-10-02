3116af2d5f29cbdaf52e7ce78d049301e6d2f7f7
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-10-02 13:53:30 +0000

    2 Oct 2023, 19:23

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -21,6 +21,7 @@ const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
+  console.log("The id is",id)
   req.todo = await Todo.findById(id)
   if (!req.todo) return res.sendStatus(404)
 
