1babdc16faa8f91c7d5b2f3b3b57427f79e2cd32
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-10-02 14:38:12 +0000

    2 Oct 2023, 20:08

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -4,6 +4,7 @@ const { MONGO_URL } = require('../util/config')
 
 if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
+console.log("mongourl is ",MONGO_URL)
 
 module.exports = {
   Todo
